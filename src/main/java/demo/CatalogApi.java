a0b7482f89d80f111a78091d4855b913032a9a0b
Julien Ponge <jponge@redhat.com>
2021-04-08 19:16:17 +0200

    Log all the things

--- src/main/java/demo/CatalogApi.java
+++ src/main/java/demo/CatalogApi.java
@@ -2,6 +2,8 @@ package demo;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
@@ -12,26 +14,32 @@ import javax.ws.rs.PathParam;
 @Path("/products")
 public class CatalogApi {
 
+  private static final Logger logger = LoggerFactory.getLogger(CatalogApi.class);
+
   @GET
   public Multi<Product> list() {
+    logger.info("list");
     return Product.all();
   }
 
   @POST
   @Transactional
   public Uni<Product> register(Product product) {
+    logger.info("register");
     return product.persistAndFlush().replaceWith(product);
   }
 
   @GET
   @Path("/{id}")
   public Uni<Product> byId(@PathParam("id") Long id) {
+    logger.info("byId");
     return Product.findById(id);
   }
 
   @GET
   @Path("/named/{name}")
   public Uni<Product> find(@PathParam("name") String name) {
+    logger.info("find");
     return Product.findByName(name);
   }
 }
